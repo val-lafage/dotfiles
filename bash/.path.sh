9da76229acadc146cd862f543b84b7714e6c0006
Valentin Lafage <valentin.lafage@celadodc-rswl.com>
2025-09-21 09:43:56 +0200

    feat: add android sdk tools
    Signed-off-by: Valentin Lafage <valentin.lafage@celadodc-rswl.com>

--- bash/.path.sh
+++ bash/.path.sh
@@ -20,6 +20,23 @@ esac
 
 [[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && \. "$SDKMAN_DIR/bin/sdkman-init.sh"
 
+case ":$PATH:" in
+  *":$ANDROID_SDK_ROOT/cmdline-tools/latest/bin:"*) ;;
+  *) export PATH="$ANDROID_SDK_ROOT/cmdline-tools/latest/bin:$PATH" ;;
+esac
+
+case ":$PATH:" in
+  *":$ANDROID_SDK_ROOT/build-tools/34.0.0:"*) ;;
+  *) export PATH="$ANDROID_SDK_ROOT/build-tools/34.0.0:$PATH" ;;
+esac
+
+# TOOLS
+
+case ":$PATH:" in
+  *":$RSWL/tools/scripts:"*) ;;
+  *) export PATH="$RSWL/tools/scripts:$PATH" ;;
+esac
+
 # JAVA
 
 if command -v java >/dev/null 2>&1; then
